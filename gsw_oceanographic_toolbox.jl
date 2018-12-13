1b80af0d39dded1c715dc9118b95c34e71bba537
axline <axline@mail.ru>
2018-12-13 10:11:28 +0700

    Initial commit

--- gsw_oceanographic_toolbox.jl
+++ gsw_oceanographic_toolbox.jl
@@ -1190,12 +1190,11 @@ end
 Copyright (C) 2018 Alexander Smirnov (axline@mail.ru)
 
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
+the terms of the GNU General Public License 3 as published by the Free Software
+Foundation. See the GNU General Public License for more details
+(http://www.gnu.org/licenses/).
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-details (http://www.gnu.org/licenses/).
+FITNESS FOR A PARTICULAR PURPOSE.
 """
